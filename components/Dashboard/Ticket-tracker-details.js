e15ed4b81091afacaed9886a0bd6e4d68b76ae1c
Parker <parkerproject@gmail.com>
2017-04-09 13:04:56 -0400

    updated

--- components/Dashboard/Ticket-tracker-details.js
+++ components/Dashboard/Ticket-tracker-details.js
@@ -31,9 +31,6 @@ class TicketTrackerDetails extends Component {
   }
 
   render() {
-    if (this.state.sales.length === 0) {
-      return <div>No sales</div>;
-    }
     const totalAmtSold = this.totalSales();
     const remaining = 15 - totalAmtSold;
     return (
@@ -78,9 +75,15 @@ class TicketTrackerDetails extends Component {
           </div>
           <div className="tracker_buttons">
             <div className="container">
-              <a href="" className="opentracker">
-                BUYOUT TICKETS
-              </a>
+              <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                <input type="hidden" name="cmd" value="_s-xclick" />
+                <input type="hidden" name="hosted_button_id" value="5QCP4S5GAVHJE" />
+                <button type="submit" className="opentracker">
+                  BUYOUT TICKETS
+                </button>
+                {/* <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" /> */}
+                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+              </form>
             </div>
           </div>
         </div>
